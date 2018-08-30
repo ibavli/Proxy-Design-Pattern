29cfd92a3715c211b55372a5f2e565fab3fb9f3c
ibavli <ibrahim.bavli35@gmail.com>
2018-08-30 20:18:38 +0300

    Simple proxy design pattern example.

--- ProxyDesignPattern/Program.cs
+++ ProxyDesignPattern/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProxyDesignPattern
@@ -11,6 +12,46 @@ namespace ProxyDesignPattern
         static void Main(string[] args)
         {
 
+            //Faktoriel _faktoriel = new Faktoriel();
+            //Console.WriteLine(_faktoriel.Hesapla());
+            //Console.WriteLine(_faktoriel.Hesapla());
+
+            SayiAbstract faktorielproxy = new FaktroielProxy();
+            Console.WriteLine(faktorielproxy.Hesapla());
+            Console.WriteLine(faktorielproxy.Hesapla());
+
+            Console.ReadKey();
+        }
+    }
+    abstract class SayiAbstract
+    {
+        public abstract int Hesapla();
+    }
+    class Faktoriel : SayiAbstract
+    {
+        public override int Hesapla()
+        {
+            int sayi = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                sayi *= i;
+                Thread.Sleep(1000);
+            }
+            return sayi;
+        }
+    }
+    class FaktroielProxy : SayiAbstract
+    {
+        private Faktoriel faktoriel;
+        private int _cachesayi;
+        public override int Hesapla()
+        {
+            if (faktoriel == null)
+            {
+                faktoriel = new Faktoriel();
+                _cachesayi = faktoriel.Hesapla();
+            }
+            return _cachesayi;
         }
     }
 }
